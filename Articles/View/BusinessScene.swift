682fa78fca10208ba2995e4292ef12a39ce3676c
LouiseMustafaeva <112573505+LouiseMustafaeva@users.noreply.github.com>
2023-12-18 16:51:04 +0500

    Create new scenes: BusinessScene and SportScene

---
+++ Articles/View/BusinessScene.swift
@@ -0,0 +1,20 @@
+//
+//  BusinessScene.swift
+//  Articles
+//
+//  Created by Louise on 18/12/23.
+//
+
+import SwiftUI
+
+struct BusinessScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BusinessScene_Previews: PreviewProvider {
+    static var previews: some View {
+        BusinessScene()
+    }
+}
---
+++ Articles/View/SportScene.swift
@@ -0,0 +1,20 @@
+//
+//  SportScene.swift
+//  Articles
+//
+//  Created by Louise on 18/12/23.
+//
+
+import SwiftUI
+
+struct SportScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SportScene_Previews: PreviewProvider {
+    static var previews: some View {
+        SportScene()
+    }
+}
