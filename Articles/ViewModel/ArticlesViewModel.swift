0423122d4ed969c321c526161b78d364074f5183
LouiseMustafaeva <112573505+LouiseMustafaeva@users.noreply.github.com>
2023-12-18 16:59:43 +0500

    Update saving data in Core Data

--- Articles/ViewModel/ArticlesViewModel.swift
+++ Articles/ViewModel/ArticlesViewModel.swift
@@ -16,32 +16,30 @@ class ArticlesViewModel: ObservableObject {
     
     func getData(screenType: ScreenType, items: FetchedResults<Item>, viewContext: NSManagedObjectContext) {
         networkManager.doRequest(type: screenType) { result in
-            
-            if screenType != .home {
-                for i in items {
-                    viewContext.delete(i)
-                }
-                do {
-                    try viewContext.save()
-                } catch {
-                    let error = error as Error
-                    fatalError("Unresolved error \(error), \(error.localizedDescription)")
-                }
-            }
-            
+//                self.deleteAll(items: items, viewContext: viewContext)
             DispatchQueue.main.async {
                 for article in result.articles ?? [] {
                     let newItem = Item(context: viewContext)
                     newItem.title = article.title
-                    newItem.subTitle = article.content
+                    newItem.subTitle = article.description
                     newItem.imageUrl = article.urlToImage
+                    newItem.author = article.author
+                    newItem.date = article.publishedAt
                     
-                    if !items.contains(newItem) {
+                    if !items.contains(where: {$0.title == newItem.title}) {
                         do {
                             try viewContext.save()
                         } catch {
                             let nsError = error as NSError
                             fatalError("Unresolved error \(nsError), \(nsError.localizedDescription)")
+                        }
+                    } else {
+                        viewContext.delete(newItem)
+                        do {
+                            try viewContext.save()
+                        } catch {
+                            let error = error as Error
+                            fatalError("Unresolved error \(error), \(error.localizedDescription)")
                         }
                     }
                 }
@@ -49,7 +47,33 @@ class ArticlesViewModel: ObservableObject {
         }
     }
     
+    func deleteAll(items: FetchedResults<Item>, viewContext: NSManagedObjectContext) {
+        for i in items {
+            viewContext.delete(i)
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            let error = error as Error
+            fatalError("Unresolved error \(error), \(error.localizedDescription)")
+        }
+    }
+    
     func getDataFromNetwork(screenType: ScreenType) {
-        
+        networkManager.doRequest(type: screenType) { result in
+            if screenType == .sport {
+                for article in result.articles ?? [] {
+                    if !self.sportNews.contains(article) {
+                        self.sportNews.append(article)
+                    }
+                }
+            } else {
+                for article in result.articles ?? [] {
+                    if !self.businessNews.contains(article) {
+                        self.businessNews.append(article)
+                    }
+                }
+            }
+        }
     }
 }
