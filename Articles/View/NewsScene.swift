5f0deea8c08c9c46071e2ef994072ad3f8a7f627
LouiseMustafaeva <112573505+LouiseMustafaeva@users.noreply.github.com>
2023-12-18 16:44:23 +0500

    Add DetailScene on NewsScene

--- Articles/View/NewsScene.swift
+++ Articles/View/NewsScene.swift
@@ -18,17 +18,21 @@ struct NewsScene: View {
     var screenType: ScreenType
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items){ item in
-                    NewsCell(title: item.title ?? "", subTitle: item.subTitle ?? "", imageUrl: item.imageUrl ?? "")
-                        .listRowInsets(EdgeInsets())
-                        .listRowSeparator(.hidden)
+                List {
+                    ForEach(items){ item in
+                        NavigationLink {
+                            DetailScene(imageUrl: item.imageUrl ?? "", title: item.title ?? "", author: item.author ?? "", date: item.date ?? "", content: item.subTitle ?? "")
+                        } label: {
+                            NewsCell(title: item.title ?? "", subTitle: item.subTitle ?? "", imageUrl: item.imageUrl ?? "")
+                                .listRowInsets(EdgeInsets())
+                                .listRowSeparator(.hidden)
+                        }.navigationBarTitleDisplayMode(.large)
+                    }
+                }.onAppear{
+                    let viewModel = ArticlesViewModel()
+                    viewModel.getData(screenType: screenType, items: items, viewContext: viewContext)
                 }
-            }.onAppear{
-                let viewModel = ArticlesViewModel()
-                viewModel.getData(screenType: screenType, items: items, viewContext: viewContext)
-            }
-            .scrollContentBackground(.hidden)
+                .scrollContentBackground(.hidden)
                 .navigationTitle("News")
         }
     }
